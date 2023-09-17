e39fa5e4f5d3fbfb7adb442082cd7f35221e5dd6
Murillo Ribeiro <69175791+muriklo@users.noreply.github.com>
2023-09-17 20:31:41 -0300

    Update rev01.asm

--- mars/rev01.asm
+++ mars/rev01.asm
@@ -13,68 +13,65 @@ A:	.word 1 , 2 , 3 , 5 , 8
 B:	.word 13, 21, 34, 55, 89
 
 .text 
-li $t0, 0			# x = 0
-li $t1, 0			# f = 0
-li $t2, 2			# g = 2
-li $t3, 4			# h = 4
-li $t4, 0			# y = 0
+li $t0, 0			# f = 0
+li $t1, 1			# g = 1
+li $t2, 2			# h = 2
+li $t3, 4			# i = 4
+li $t4, 5			# j = 5
 
 ######################################
 # f = ((g+1) * h) - 3
 
-addi $t1, $t2, 1  		# g + 1
-mul $t0, $t1, $t3 		# (g + 1) * h
+addi $t1, $t1, 1  		# g + 1
+mul $t0, $t1, $t2 		# (g + 1) * h
 li $t5, 3         		# $t5 = 3
-sub $t1, $t0, $t5		# f = ((g + 1) * h) - 3
+sub $t0, $t0, $t5		  # f = ((g + 1) * h) - 3
 
 ######################################
 # f = (h*h + 2) / f - g
 
-mul $t0, $t3, $t3		# h * h
-addi $t0, $t0, 2		# h * h + 2
-sub $t5, $t1, $t2		# f - g
-div $t1, $t0, $t5		# (h * h + 2) / f - g
+mul $t5, $t2, $t2		# h * h
+addi $t5, $t5, 2		# h * h + 2
+sub $t6, $t0, $t1		# f - g
+div $t0, $t5, $t6		# f = (h * h + 2) / f - g
 
 ######################################
 # B[i] = 2 * A[i] 
 
-li $t6, 1			# i = 1
-sll $t6, $t6, 2			# i * 4
+sll $t6, $t3, 2			# i * 4
 la $t5, A			
 add $t5, $t5, $t6		# endereço + 4 para acessar A[1]
 lw $t5, 0($t5)			# $t5 = A[1]
 sll $t5, $t5, 1			# 2 * A[1]
-la $t7, B			# carrega endereço base de B
+la $t7, B			      # carrega endereço base de B
 add $t6, $t6, $t7		# endereço + 4 para acessar B[1]
 sw $t5, 0($t6)			# B[1] = 2 * A[1]
 
 ######################################
 # B[f+g] = A[i] / (A[j] - B[j])
 
-add $t5, $t1, $t2		# f + g
+add $t5, $t0, $t1		# f + g
 sll $t5, $t5, 2			# 4 * (f + g)
-li  $t6, 1			# i = 1
-sll $t6, $t6, 2			# i * 4
-li  $t7, 2			# j = 2
-sll $t7, $t7, 2			# j * 4
+sll $t6, $t3, 2			# i * 4
+sll $t7, $t4, 2			# j * 4
 
-la $t8, A			# endereço base de A
-add $t9, $t8, $t7		# endereço + 8 para acessar A[2]
-lw $t8, 0($t9)			# $t8 = A[2]
+la $t8, A			      # endereço base de A
+add $t9, $t8, $t7		# endereço + 4 * j para acessar A[j]
+lw $t8, 0($t9)			# $t8 = A[j]
 
-la $t9, B			# endereço base de B
-add $t9, $t9, $t7		# endereço + 8 para acessar B[2]
-lw $t9, 0($t9)			# $t9 = B[2]
+la $t9, B			      # endereço base de B
+add $t9, $t9, $t7		# endereço + 4 * j para acessar B[j]
+lw $t9, 0($t9)			# $t9 = B[j]
 
-sub $t9, $t8, $t9		# A[2] - B[2]
+sub $t9, $t8, $t9		# A[j] - B[j]
 
 la $t8, A
-add $t8, $t8, $t6		# endereço + 4 para acessar A[1]
-lw $t8, 0($t8)			# $t8 = A[1]
+add $t8, $t8, $t6		# endereço + 4 * i para acessar A[i]
+lw $t8, 0($t8)			# $t8 = A[i]
 
-div $t8, $t8, $t9		# A[1] / A[2] - B[2]
-mflo $t8			# $t8 = A[1] / A[2] - B[2]
+div $t8, $t8, $t9		# A[i] / A[j] - B[j]
+mflo $t8			      # $t8 = A[i] / A[j] - B[j]
 
-la $t6, B			# endereço base de B
+la $t6, B			      # endereço base de B
 add $t6, $t6, $t5		# endereço + 4 * (f + g) para acessar B[f + g]
-sw $t8, 0($t6)			# B[f + g] = A[1] / A[2] - B[2]
+sw $t8, 0($t6)			# B[f + g] = A[i] / A[j] - B[j]
