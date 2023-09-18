b82e1cb0bd86f397d2ed556797a44ecdff2a7b1e
Murillo Ribeiro <69175791+muriklo@users.noreply.github.com>
2023-09-18 10:51:35 -0300

    Update rev02.asm

--- mars/rev02.asm
+++ mars/rev02.asm
@@ -7,10 +7,10 @@
 # a: $s0, b: $s1, c: $s2, d: $s3
 #########################################################
 
-li $s0, 1		# a = 1
-li $s1, 2		# b = 2
-li $s2, 4		# c = 4
-li $s3, 8		# d = 8
+li $s0, 1			# a = 1
+li $s1, 2			# b = 2
+li $s2, 4			# c = 4
+li $s3, 8			# d = 8
 
 #########################################################
 # if (a != b) {
@@ -24,16 +24,17 @@ li $s3, 8		# d = 8
 #    }
 # }
 #########################################################
-li, $t0, 3
 
-	beq $s0, $s1, END
-	move $s0, $s1
-	blt $s2, 3, IF_menor
+	li, $t0, 3		# $t0 carregado com 3 para comparar com C ou inicializar o FOR
+
+	beq $s0, $s1, END	# verifica se a!= b, caso igual, encerra o programa
+	move $s0, $s1		# realiza a = b;
+	blt $s2, 3, IF_menor	# verifica se c < 3, caso menor, pula para IF_menor
 FOR: 
-	beq $t0, 15, END
-	add $s1, $t0, $s1
-	addi $t0, $t0, 2
-	j FOR
+	bge $t0, 15, END	# condição para finalizar o loop do for, quando i >= 15
+	add $s1, $t0, $s1	# realiza b += i;
+	addi $t0, $t0, 2	# incrementa i += 2;
+	j FOR			# retorna o loop do FOR
 IF_menor:
-	addi $s0, $s0, 1
+	addi $s0, $s0, 1	# incrementa a++;
 END:
